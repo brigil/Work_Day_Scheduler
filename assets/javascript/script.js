1f1411292f616a579637f60f767df7b90c498989
Brigitte G <urbrigette@gmail.com>
2020-09-19 10:39:47 -0400

    testing a for loop

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,12 @@
 // Display current time using Moment.js
 // jquery displaying the current time by connecting to the id "currentDay"
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+// Create variables
+var currentTime = moment().hour();
+
+for (let hour = 1; hour < 10; hour ++){
+    console.log(hour)
+}
 // Click event to save text area content to Local Storage
 
 // create 9 rows with 3 columns
